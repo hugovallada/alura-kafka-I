9bf178d116784a1fdcfc64f9da55dabd60dbc942
hugovallada <valladahugo@gmail.com>
2021-05-17 08:49:30 -0300

    Extraindo camadas no consumer e producer

--- src/main/java/com/github/hugovallada/kafka/FraudDetectorService.java
+++ src/main/java/com/github/hugovallada/kafka/FraudDetectorService.java
@@ -1,24 +1,17 @@
 package com.github.hugovallada.kafka;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Properties;
-import java.util.UUID;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+
+
 
 public class FraudDetectorService {
     public static void main(String[] args) {
         var fraudService = new FraudDetectorService();
-        var service = new KafkaService(EmailService.class.getSimpleName(),"ECOMMERCE_NEW_ORDER", fraudService::parse);
-        service.run();
+        try(var service = new KafkaService(EmailService.class.getSimpleName(),"ECOMMERCE_NEW_ORDER", fraudService::parse)) {
+            service.run();
+        }
     }
 
     private void parse(ConsumerRecord<String, String> record) {
